c121f955f9d857c82b49a1332919f6a72ddb7b8e
NickM <nmikstas@alumni.scu.edu>
2020-03-15 16:28:07 -0600

    Added the styling for the 2d renderer

--- assets/js/ntRenderTest.js
+++ assets/js/ntRenderTest.js
@@ -13,6 +13,17 @@ window.addEventListener("resize", () =>
     ntRenderer.resize();
 });
 
+/************************************** Disable Key Scrolling **************************************/
+
+window.addEventListener("keydown", (e) =>
+{
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1)
+    {
+        e.preventDefault();
+    }
+}, false);
+
 /*************************************** Button Listeners ****************************************/
 
 $( document ).ready(function()
